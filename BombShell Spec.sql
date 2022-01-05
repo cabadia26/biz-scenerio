712147bce0d9c03af59c662357ee5ffefeaeafb0
cabadia26 <cabadia26@gmail.com>
2022-01-04 19:08:38 -0500

    #19

---
+++ BombShell Spec.sql
@@ -0,0 +1,40 @@
+ /*Hi I own a store called BombShell and we sell top quality cotton shells.
+Having to constantly count my inventory and running the store chasing costs and profit margins is taking too much time 
+and so my partner and I decided to go electronically. 
+I was referred to you as I was told you were the best to reach out for help.
+
+I have many colors including
+white, black, nude, yellow, gray, brown, pink, purple, blue, and navy 
+
+I have 2 of each size -small, medium, and large
+
+I purchase them at wholesale for 6$ each and sell them for 12.99$ each
+
+Each shell has its own SKU number so that i can differentiate when checking all my inventory and count
+
+I have two styles in each color - ''Elbow'' and ''wrist'' 
+
+Questions:
+1.Do you sell any other fabrics?
+no, we only sell the top quality Eygptian cotton and thats what the clients love from my shells. 
+
+2. Would you ever sell it less than you bought it for?
+no
+
+3. Do you need to store from who/which company you purchased the shells from and how much?
+for now, I am buying from the same seller.
+ 
+
+Reports:
+1. A daily printout every morning including the Inventory in stock, style, color, size,
+date purchased, price purchased, date sold, price sold, and the date and time record of that printout to refer if i had to give a hardcopy to my partner.
+
+2. Printout of sku, style, color, size, date purchase, price purchase, date sold, price sold
+
+3. which style is giving me most profit 
+
+4. which size sells the most
+
+5. avg how long does it take for inventory to be sold per color each week
+
+
