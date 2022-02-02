06365b524641da85fe1d9a307e66d55d046bf7a8
SuriRose <92594843+SuriRose@users.noreply.github.com>
2022-02-01 21:00:37 -0500

    Update 4 reports.sql

--- 4 reports.sql
+++ 4 reports.sql
@@ -4,6 +4,11 @@
 --date purchased, price purchased, date sold, price sold, and the date and time record of that printout to refer if i had to give a hardcopy to my partner.
 select b.style, b.color, b.size, b.datepurchased, b.pricepurchased, b.Datesold, b.pricesold, b.DateTimeSaved
 from BombShell b 
+where b.datesold is not null
+
+select b.style, b.color, b.size, b.datepurchased, b.pricepurchased, b.Datesold, b.pricesold, b.DateTimeSaved
+from BombShell b 
+where b.datesold isnull
 --2. Printout of sku, style, color, size, date purchase, price purchase, date sold, price sold
 select BombShellId, b.style, b.color, b.size, b.datepurchased, b.pricepurchased, b.datesold, b.pricesold
 from BombShell b 
@@ -18,8 +23,7 @@ group by b.size
 --5. avg how long does it take for inventory to be sold per color each week
 select HowQuickSell = avg(datediff(DAY, b.datepurchased, b.Datesold))
 from BombShell b 
-where datediff(DAY, b.datepurchased, b.Datesold) <= 7
+where datediff(week, b.datepurchased, b.Datesold) <= 1
 group by b.color, b.datepurchased, b.Datesold
-
 
 
